578f3be5439b0d92b6ec6681e5507098e377c500
Trevor Kimenye <kimenye@gmail.com>
2014-05-27 17:14:27 +0300

    commented

--- js/app.js
+++ js/app.js
@@ -1,88 +1,92 @@
 $(document).ready(function() {
+
+    var pattern = /^((http|https|ftp):\/\/)/;
+
+    
     // $.fn.fullpage();
 
-    $(document).foundation({
+    // $(document).foundation({
 
-        orbit: {
-            animation: 'slide',
-            timer: false,
-            next_on_click: false,
-            animation_speed: 1000,
-            navigation_arrows: true,
-            bullets: false,
-            swipe: false
-       }
-    });
+    //     orbit: {
+    //         animation: 'slide',
+    //         timer: false,
+    //         next_on_click: false,
+    //         animation_speed: 1000,
+    //         navigation_arrows: true,
+    //         bullets: false,
+    //         swipe: false
+    //    }
+    // });
 
-    function setToken(token) {
-    	$('#code').html(token);
-    }
+    // function setToken(token) {
+    // 	$('#code').html(token);
+    // }
 
-    function handleMessage(message) {
+    // function handleMessage(message) {
 
-        if (!verified) {
-            if (message.data.message == token) {                
-                $('input[name="phone_number"]').val(message.data.from);
-                $('.instructions h1, .navigation a.go').addClass('success');
-                $('.navigation a.go').removeClass('disabled').html('Lets go!');
+    //     if (!verified) {
+    //         if (message.data.message == token) {                
+    //             $('input[name="phone_number"]').val(message.data.from);
+    //             $('.instructions h1, .navigation a.go').addClass('success');
+    //             $('.navigation a.go').removeClass('disabled').html('Lets go!');
 
-                verified = true;
-            }
-        }
-    }
+    //             verified = true;
+    //         }
+    //     }
+    // }
 
-    function getToken(reset) {
-        $.getJSON("demo/code", function(data) {
-            token = data.code;
-            setToken(token);
-        });
-    }   
+    // function getToken(reset) {
+    //     $.getJSON("demo/code", function(data) {
+    //         token = data.code;
+    //         setToken(token);
+    //     });
+    // }   
 
-    var token = null, step = 0, verified = false;
-    if (token == null) {
-    	getToken(false);
-    }
-    else {
-    	setToken(token);
-    }
+    // var token = null, step = 0, verified = false;
+    // if (token == null) {
+    // 	getToken(false);
+    // }
+    // else {
+    // 	setToken(token);
+    // }
 
-    $('#sendForm').submit(function() { 
-        // submit the form 
-        $(this).ajaxSubmit({ success: function() {
-                $('small.sent').removeClass('hidden');
-                $('textarea[name="message"]').val('');
-            }
-        }); 
-        // return false to prevent normal browser submit and page navigation 
-        return false; 
-    });
+    // $('#sendForm').submit(function() { 
+    //     // submit the form 
+    //     $(this).ajaxSubmit({ success: function() {
+    //             $('small.sent').removeClass('hidden');
+    //             $('textarea[name="message"]').val('');
+    //         }
+    //     }); 
+    //     // return false to prevent normal browser submit and page navigation 
+    //     return false; 
+    // });
 
-    $('.compose textarea').keyup(function() {
-        if (!$('small.sent').hasClass('hidden'))
-            $('small.sent').addClass('hidden');    
-    });
+    // $('.compose textarea').keyup(function() {
+    //     if (!$('small.sent').hasClass('hidden'))
+    //         $('small.sent').addClass('hidden');    
+    // });
 
-    $('.navigation a').click(function() {
-        if (verified)
-            $('.orbit-next').click();
-    });
+    // $('.navigation a').click(function() {
+    //     if (verified)
+    //         $('.orbit-next').click();
+    // });
 
-    $('.restart').click(function() {
-        token = null;
-        verified = false;
-        getToken(true);
-    });
+    // $('.restart').click(function() {
+    //     token = null;
+    //     verified = false;
+    //     getToken(true);
+    // });
 
-    var pubnub = PUBNUB.init({
-        publish_key: 'pub-c-d2bcacda-ad1e-4c33-aa0f-77e592c387da',
-        subscribe_key: 'sub-c-ebfa3f62-71f4-11e2-93fe-12313f022c90'
-    });
+    // var pubnub = PUBNUB.init({
+    //     publish_key: 'pub-c-d2bcacda-ad1e-4c33-aa0f-77e592c387da',
+    //     subscribe_key: 'sub-c-ebfa3f62-71f4-11e2-93fe-12313f022c90'
+    // });
 
-    pubnub.subscribe({
-        channel: 'ongair_im',
-        message: function(m){
-            // console.log(m)
-            handleMessage(m);
-        }
-    });
+    // pubnub.subscribe({
+    //     channel: 'ongair_im',
+    //     message: function(m){
+    //         // console.log(m)
+    //         handleMessage(m);
+    //     }
+    // });
 });
